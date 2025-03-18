97148a3ac6324842831e757f7f78d93aea89b88f
merlinovvv <merlinovandrej@gmail.com>
2025-03-18 19:07:13 +0200

    ver 1.0.9

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 add_action('wp_enqueue_scripts', 'concordia_styles');
 add_action('wp_enqueue_scripts', 'concordia_scripts');
 
-$version = '1.0.8';
+$version = '1.0.9';
 
 function concordia_styles()
 {
